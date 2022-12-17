e72da018d4b86cbb2da9599fe369548713032059
faludiz <zoltan@faludi.info>
2022-12-18 00:43:51 +0100

    move crs-defs to resource

--- source/crs.pas
+++ source/crs.pas
@@ -8,89 +8,91 @@ uses
   Classes, SysUtils;
 
 type
-  TCRS=record
-    id:string;
-    def:string;
+  TCRS = record
+    id:  string;
+    def: string;
   end;
 
   TCRSList = array of TCRS;
 
-procedure LoadCRS(const fn:string; var lst:TCRSList);
-function GetCRSIndex(const id:string; const lst:TCRSList):integer;
-function GetCRSDef(const id:string; const lst:TCRSList):string;
-function GetCRSIDList(const lst:TCRSList):tstringlist;
-function AddCRS(const c:tcrs; var lst:TCRSList):integer;
+procedure LoadCRS(var lst: TCRSList);
+function GetCRSIndex(const id: string; const lst: TCRSList): integer;
+function GetCRSDef(const id: string; const lst: TCRSList): string;
+function GetCRSIDList(const lst: TCRSList): TStringList;
+function AddCRS(const c: tcrs; var lst: TCRSList): integer;
 
 implementation
 
 uses
-  inifiles;
+  inifiles, lcltype;
 
-procedure LoadCRS(const fn: string; var lst: TCRSList);
+procedure LoadCRS(var lst: TCRSList);
 var
-  ini:tinifile;
-  sec:tstringlist;
-  def:string;
-  i:integer;
-  c:tcrs;
+  ini: tinifile;
+  res: TResourceStream;
+  sec: TStringList;
+  def: string;
+  i:   integer;
+  c:   tcrs;
 const
-  sss = 'CRS-Defs';
+  sss = 'CRS-DEFS';
 begin
-  ini:=tinifile.Create(fn);
-  sec:=tstringlist.Create;
-  ini.ReadSection(sss,sec);
-  for i:=0 to sec.Count-1 do begin
-    def:=ini.ReadString(sss, sec[i], '');
-    if def<>'' then begin
-      c.id:=sec[i];
-      c.def:=def;
+  res := TResourceStream.Create(HInstance, sss, RT_RCDATA);
+  ini := tinifile.Create(res);
+  sec := TStringList.Create;
+  ini.ReadSection(sss, sec);
+  for i := 0 to sec.Count - 1 do begin
+    def := ini.ReadString(sss, sec[i], '');
+    if def <> '' then begin
+      c.id  := sec[i];
+      c.def := def;
       addcrs(c, lst);
     end;
   end;
   sec.Free;
   ini.Free;
+  res.Free;
 end;
 
-function GetCRSIndex(const id: string; const lst:TCRSList): integer;
+function GetCRSIndex(const id: string; const lst: TCRSList): integer;
 var
-  i:integer;
+  i: integer;
 begin
-  result:=-1;
-  for i:=0 to length(lst)-1 do if upcase(id)=upcase(lst[i].id) then begin
-    result:=i;
-    break;
-  end;
+  Result := -1;
+  for i := 0 to length(lst) - 1 do if upcase(id) = upcase(lst[i].id) then begin
+      Result := i;
+      break;
+    end;
 end;
 
 function GetCRSDef(const id: string; const lst: TCRSList): string;
 var
-  idx:integer;
+  idx: integer;
 begin
-  result:='';
-  idx:=GetCRSIndex(id, lst);
-  if idx>=0 then result:=lst[idx].def;
+  Result := '';
+  idx    := GetCRSIndex(id, lst);
+  if idx >= 0 then Result := lst[idx].def;
 end;
 
-function GetCRSIDList(const lst: TCRSList): tstringlist;
+function GetCRSIDList(const lst: TCRSList): TStringList;
 var
-  i:integer;
+  i: integer;
 begin
-  result:=tstringlist.Create;
-  for i:=0 to length(lst)-1 do result.Add(lst[i].id);
+  Result := TStringList.Create;
+  for i := 0 to length(lst) - 1 do Result.Add(lst[i].id);
 end;
 
 function AddCRS(const c: tcrs; var lst: TCRSList): integer;
 var
-  idx:integer;
+  idx: integer;
 begin
-  idx:=GetCRSIndex(c.id, lst);
-  if idx<0 then begin
+  idx := GetCRSIndex(c.id, lst);
+  if idx < 0 then begin
     setlength(lst, length(lst) + 1);
-    idx:=length(lst)-1;
+    idx := length(lst) - 1;
   end;
-  lst[idx]:=c;
-  result:=idx;
+  lst[idx] := c;
+  Result   := idx;
 end;
 
 end.
-
